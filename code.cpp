49cc309c2e77cbbdeeb4268029b1ffa1966c8665
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-03-09 02:03:26 +0600

    second commit

---
+++ code.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define pop pop_back()
+#define len(a) sizeof(a)
+#define lc (n*2)
+#define rc ((n*2)+1)
+#define all(x) (x).begin, (x).end()
